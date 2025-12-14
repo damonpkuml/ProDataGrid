c557bde84a2896360d1110113e1ca2325ce4298d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-12-14 12:25:18 +0100

    Adjust headers

--- src/Avalonia.Controls.DataGrid/DataGridComboBoxColumn.cs
+++ src/Avalonia.Controls.DataGrid/DataGridComboBoxColumn.cs
@@ -1,7 +1,5 @@
-﻿// (c) Copyright Microsoft Corporation.
-// This source is subject to the Microsoft Public License (Ms-PL).
-// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
-// All other rights reserved.
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 
 #nullable disable
 
